4e104019549420352f37c50141c15955e07e936f
Dikshant <dikshantk809@gmail.com>
2026-06-03 11:51:45 +0530

    Add files via upload

---
+++ C++/code.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "hello";
+    return 0;
+}
+
+
+
+// Boilerplate Code:
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   
+    return 0;
+}
